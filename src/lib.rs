e8d27c62a730224026c07c3c5f233f935a66498f
f3kilo <f3kilo@yandex.ru>
2020-08-19 10:48:07 +0300

    Graphics struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
 pub mod back;
 
+#[macro_use]
+extern crate slog;
+
+use crate::back::GraphicsBackend;
+use slog::Logger;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+pub struct Graphics {
+    logger: Logger,
+    backend: Arc<Mutex<Box<dyn GraphicsBackend>>>,
+}
+
+impl Graphics {
+    pub fn new(backend: Box<dyn GraphicsBackend>, logger: Option<Logger>) -> Self {
+        let backend = Arc::new(Mutex::new(backend));
+        let logger = logger.unwrap_or_else(|| Logger::root(slog::Discard, o!()));
+        Self { backend, logger }
+    }
+
+    pub fn replace_backend(
+        &mut self,
+        new_backend: Box<dyn GraphicsBackend>,
+    ) -> Box<dyn GraphicsBackend> {
+        let mut mut_back = self.backend.lock().unwrap();
+        std::mem::replace(&mut mut_back, new_backend)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
