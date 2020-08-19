9dbcf074c5262df4d80814e6ea7009a99a3b432c
f3kilo <f3kilo@yandex.ru>
2020-08-19 14:17:19 +0300

    Add second backend and replace first with it.

--- examples/basics/main.rs
+++ examples/basics/main.rs
@@ -1,20 +1,24 @@
 mod first_back;
+mod second_back;
 
 use f3_gfx::Graphics;
 #[macro_use]
 extern crate slog;
 use crate::first_back::FirstBack;
+use crate::second_back::SecondBack;
 use f3_gfx::back::GraphicsBackend;
 use f3_gfx::texture::Texture;
 use slog::{Drain, Logger};
 
 fn main() {
     let logger = init_logger();
-    let back = init_backend(logger.clone());
-    let graphics = Graphics::new(back, Some(logger));
+    let back = init_first_backend(logger.clone());
+    let mut graphics = Graphics::new(back, Some(logger.clone()));
 
-    let tex1 = Texture::new("First tex path".into(), graphics.get_texture_manager()).unwrap();
-    let tex2 = Texture::new("Second tex path".into(), graphics.get_texture_manager()).unwrap();
+    use_graphics(&graphics);
+
+    graphics.replace_backend(init_second_backend(logger.clone()));
+    use_graphics(&graphics)
 }
 
 fn init_logger() -> Logger {
@@ -22,6 +26,15 @@ fn init_logger() -> Logger {
     Logger::root(slog_term::FullFormat::new(plain).build().fuse(), o!())
 }
 
-fn init_backend(logger: Logger) -> Box<dyn GraphicsBackend> {
+fn init_first_backend(logger: Logger) -> Box<dyn GraphicsBackend> {
     Box::new(FirstBack::new(logger))
+}
+
+fn init_second_backend(logger: Logger) -> Box<dyn GraphicsBackend> {
+    Box::new(SecondBack::new(logger))
+}
+
+fn use_graphics(graphics: &Graphics) {
+    let tex1 = Texture::new("First tex path".into(), graphics.get_texture_manager()).unwrap();
+    let tex2 = Texture::new("Second tex path".into(), graphics.get_texture_manager()).unwrap();
 }
